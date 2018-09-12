7e7d33a714621989b43318bccf2eb818662cd8f8
deepshikharai <37527448+deepshikharai@users.noreply.github.com>
2018-09-12 21:29:23 +0530

    Update Docker_p.java

--- PHP_traveller/src/main/java/D1/Docker_p.java
+++ PHP_traveller/src/main/java/D1/Docker_p.java
@@ -47,7 +47,7 @@ public class Docker_p {
 		dcp.setCapability("idleTimeout", 150);
 		
 		//driver = new RemoteWebDriver(new URL("http://35.199.155.4:4444/wd/hub"),dcp);
-		driver = new RemoteWebDriver(new URL("http://35.226.39.96:4444/wd/hub"),dcp);
+		driver = new RemoteWebDriver(new URL("http://35.231.161.229:4444/wd/hub"),dcp);
 	
 		/*System.setProperty("webdriver.chrome.driver","D:\\drivers\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver(); 
